2ce520094c7fe8429a5a2655436c6de3e4bc8dfc
ronit-mukherjee <connect.ronit@gmail.com>
2020-05-25 16:40:19 +0530

    tcw9nbVTHOk | React Router | Nested Routing | ILive4Coding

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Switch, Route, useRouteMatch } from "react-router-dom";
+import Service from "../Service/Service";
 
 const Services = () => {
+  const { url, path } = useRouteMatch();
   return (
     <div>
       <h2>Services</h2>
-      <ul>
-        <li>
-          <Link to="/services/1234">Service 1</Link>
-        </li>
-        <li>
-          <Link to="/services/5678">Service 2</Link>
-        </li>
-      </ul>
+
+      <Route path={path}>
+        <ul>
+          <li>
+            <Link to={`${url}/1234`}>Service 1</Link>
+          </li>
+          <li>
+            <Link to={`${url}/5678`}>Service 2</Link>
+          </li>
+        </ul>
+      </Route>
+      <Route path={`${path}/:id`} component={Service} />
     </div>
   );
 };
